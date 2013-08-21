3453e0ef24e6362bf39c1142b3dd357e9b49f62f
Alexandre Fiori <fiorix@gmail.com>
2013-08-20 23:14:57 -0400

    Using commands from dict

--- base/hdr.go
+++ base/hdr.go
@@ -7,10 +7,7 @@
 
 package base
 
-import (
-	"fmt"
-	"unsafe"
-)
+import "unsafe"
 
 type Header struct {
 	Version          uint8
@@ -20,22 +17,6 @@ type Header struct {
 	ApplicationId    uint32
 	HopByHopId       uint32
 	EndToEndId       uint32
-}
-
-// String returns the diameter header in human readable format.
-func (hdr *Header) String() string {
-	rflag := hdr.CommandFlags&0x80 > 0
-	pflag := hdr.CommandFlags&0x40 > 0
-	eflag := hdr.CommandFlags&0x20 > 0
-	tflag := hdr.CommandFlags&0x10 > 0
-	cmd := hdr.CommandName()
-	return fmt.Sprintf(
-		"%s (%s) Header{Code=%d,Version=%d,"+
-			"MessageLength=%d,CommandFlags={r=%v,p=%v,e=%v,t=%v},"+
-			"ApplicationId=%d,HopByHopId=%#v,EndToEndId=%#v}",
-		cmd.Name, cmd.Abbrev, hdr.CommandCode(), hdr.Version,
-		hdr.MessageLength(), rflag, pflag, eflag, tflag,
-		hdr.ApplicationId, hdr.HopByHopId, hdr.EndToEndId)
 }
 
 // MessageLength helper function returns RawMessageLength as int.
@@ -51,42 +32,4 @@ func (hdr *Header) SetMessageLength(length uint32) {
 // CommandCode returns RawCommandCode as int.
 func (hdr *Header) CommandCode() uint32 {
 	return uint24To32(hdr.RawCommandCode)
-}
-
-// CommandName returns the name of the command based on its code.
-func (hdr *Header) CommandName() *command {
-	var nameSuffix, abbrevSuffix string
-	if hdr.CommandFlags&0x80 > 0 {
-		nameSuffix = "-Request"
-		abbrevSuffix = "R"
-	} else {
-		nameSuffix = "-Answer"
-		abbrevSuffix = "A"
-	}
-	code := hdr.CommandCode()
-	var resp command
-	if cmd, ok := commandCodes[code]; ok {
-		resp.Name = cmd.Name + nameSuffix
-		resp.Abbrev = cmd.Abbrev + abbrevSuffix
-	} else {
-		resp.Name = "Unknown"
-		resp.Abbrev = "?"
-	}
-	return &resp
-}
-
-type command struct {
-	Name   string
-	Abbrev string
-}
-
-// TODO: Allow applications to register their commands?
-var commandCodes = map[uint32]command{
-	274: {"Abbort-Session", "AS"},
-	271: {"Accounting", "AC"},
-	257: {"Capabilities-Exchange", "CE"},
-	280: {"Device-Watchdog", "DW"},
-	282: {"Disconnect-Peer", "DP"},
-	258: {"Re-Auth", "RA"},
-	275: {"Session-Termination", "ST"},
 }
--- base/msg.go
+++ base/msg.go
@@ -32,9 +32,54 @@ func (m *Message) FindAVP(code uint32) *AVP {
 
 // PrettyPrint prints messages in a human readable format.
 func (m *Message) PrettyPrint() {
-	fmt.Println(m.Header)
+	fmt.Println(m.String())
 	for _, avp := range m.AVP {
 		fmt.Printf("  %s\n", avp)
 	}
 	fmt.Println()
+}
+
+// String returns a human readable version of the Message header.
+func (m *Message) String() string {
+	rflag := m.Header.CommandFlags&0x80 > 0
+	pflag := m.Header.CommandFlags&0x40 > 0
+	eflag := m.Header.CommandFlags&0x20 > 0
+	tflag := m.Header.CommandFlags&0x10 > 0
+	cmdName, cmdShort := findCmd(m.Dict, m.Header)
+	return fmt.Sprintf(
+		"%s (%s) Header{Code=%d,Version=%d,"+
+			"MessageLength=%d,CommandFlags={r=%v,p=%v,e=%v,t=%v},"+
+			"ApplicationId=%d,HopByHopId=%#v,EndToEndId=%#v}",
+		cmdName,
+		cmdShort,
+		m.Header.CommandCode(),
+		m.Header.Version,
+		m.Header.MessageLength(),
+		rflag, pflag, eflag, tflag,
+		m.Header.ApplicationId,
+		m.Header.HopByHopId,
+		m.Header.EndToEndId,
+	)
+}
+
+func findCmd(d *dict.Parser, h *Header) (string, string) {
+	var cmdName, cmdShort string
+	if d != nil {
+		cmd, err := d.FindCmd(h.ApplicationId, h.CommandCode())
+		if err == nil {
+			cmdName = cmd.Name
+			cmdShort = cmd.Short
+		}
+	}
+	if cmdName == "" {
+		cmdName, cmdShort = "Unknown", ""
+	}
+	if h.CommandFlags&0x80 > 0 {
+		cmdName += "-Request"
+		cmdShort += "R"
+	} else {
+		cmdName += "-Answer"
+		cmdShort += "A"
+	}
+	return cmdName, cmdShort
 }

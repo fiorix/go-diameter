3c321022bc7ed33574f22c225c26780d51c67c5f
Alexandre Fiori <fiorix@gmail.com>
2013-08-24 08:50:18 -0400

    Fixes

--- examples/helloworld/server.go
+++ examples/helloworld/server.go
@@ -27,31 +27,19 @@ func OnCER(w diam.ResponseWriter, r *diam.Request) {
 		r.Msg.Dict,
 	)
 	// Build message by attaching multiple AVPs to it.
-	m.NewAVP(
-		r.Dict.CodeFor("Result-Code"), 0x40, 0x0,
-		&base.Unsigned32{Value: 2001},
-	)
-	m.NewAVP(
-		r.Dict.CodeFor("Origin-Host"), 0x40, 0x0,
-		&base.DiameterIdentity{base.OctetString{Value: "go"}},
-	)
-	m.NewAVP(
-		r.Dict.CodeFor("Origin-Realm"), 0x40, 0x0,
-		&base.DiameterIdentity{base.OctetString{Value: "server"}},
-	)
+	m.Add(base.NewAVP(r.Dict, r.Dict.CodeFor("Result-Code"), 0x40, 0x0,
+		&base.Unsigned32{Value: 2001}))
+	m.Add(base.NewAVP(r.Dict, r.Dict.CodeFor("Origin-Host"), 0x40, 0x0,
+		&base.DiameterIdentity{base.OctetString{Value: "go"}}))
+	m.Add(base.NewAVP(r.Dict, r.Dict.CodeFor("Origin-Realm"), 0x40, 0x0,
+		&base.DiameterIdentity{base.OctetString{Value: "server"}}))
 	localIP, _, _ := net.SplitHostPort(r.LocalAddr)
-	m.NewAVP(
-		r.Dict.CodeFor("Host-IP-Address"), 0x40, 0x0,
-		&base.Address{Family: []byte("01"), IP: net.ParseIP(localIP)},
-	)
-	m.NewAVP(
-		r.Dict.CodeFor("Vendor-Id"), 0x40, 0x0,
-		&base.Unsigned32{Value: 131313},
-	)
-	m.NewAVP(
-		r.Dict.CodeFor("Product-Name"), 0x40, 0x0,
-		&base.OctetString{Value: "go-diameter"},
-	)
+	m.Add(base.NewAVP(r.Dict, r.Dict.CodeFor("Host-IP-Address"), 0x40, 0x0,
+		&base.Address{Family: []byte("01"), IP: net.ParseIP(localIP)}))
+	m.Add(base.NewAVP(r.Dict, r.Dict.CodeFor("Vendor-Id"), 0x40, 0x0,
+		&base.Unsigned32{Value: 131313}))
+	m.Add(base.NewAVP(r.Dict, r.Dict.CodeFor("Product-Name"), 0x40, 0x0,
+		&base.OctetString{Value: "go-diameter"}))
 	// Reply with the same Origin-State-Id
 	code := r.Dict.CodeFor("Origin-State-Id")
 	OriginStateId, err := r.Msg.FindAVP(code)
@@ -59,7 +47,7 @@ func OnCER(w diam.ResponseWriter, r *diam.Request) {
 		fmt.Println("Err:", err)
 		return
 	}
-	m.NewAVP(code, 0x40, 0x0, OriginStateId.Body())
+	m.Add(base.NewAVP(r.Dict, code, 0x40, 0x0, OriginStateId.Body()))
 
 	// Write response
 	fmt.Println("Response:")

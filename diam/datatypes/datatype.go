d698782cae7ed76da4ab2c17da9610bef4d572cd
Alexandre Fiori <fiorix@gmail.com>
2014-04-16 23:02:16 -0400

    Added decoding capability

--- diam/datatypes/datatype.go
+++ diam/datatypes/datatype.go
@@ -8,3 +8,44 @@ type DataType interface {
 	Serialize() []byte
 	String() string
 }
+
+type DataTypeId int
+
+const (
+	UnknownType DataTypeId = iota
+	AddressType
+	DiameterIdentityType
+	DiameterURIType
+	EnumeratedType
+	Float32Type
+	Float64Type
+	GroupedType
+	IPFilterRuleType
+	IPv4Type
+	Integer32Type
+	Integer64Type
+	OctetStringType
+	TimeType
+	UTF8StringType
+	Unsigned32Type
+	Unsigned64Type
+)
+
+var Available = map[string]DataTypeId{
+	"Address":          AddressType,
+	"DiameterIdentity": DiameterIdentityType,
+	"DiameterURI":      DiameterURIType,
+	"Enumerated":       EnumeratedType,
+	"Float32":          Float32Type,
+	"Float64":          Float64Type,
+	"Grouped":          GroupedType,
+	"IPFilterRule":     IPFilterRuleType,
+	"IPv4":             IPv4Type,
+	"Integer32":        Integer32Type,
+	"Integer64":        Integer64Type,
+	"OctetString":      OctetStringType,
+	"Time":             TimeType,
+	"UTF8String":       UTF8StringType,
+	"Unsigned32":       Unsigned32Type,
+	"Unsigned64":       Unsigned64Type,
+}

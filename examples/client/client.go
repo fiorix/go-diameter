a1d5f126a90f9aac3444557abd0ff7d1fd33fd5c
David Wilkie <dwilkie@gmail.com>
2014-05-09 12:40:52 +0700

    document using custom parsers to load multiple dictionaries. Closes #4

--- examples/client/client.go
+++ examples/client/client.go
@@ -62,6 +62,12 @@ func main() {
 // NewClient sends a CER to the server and then a DWR every 10 seconds.
 func NewClient(c diam.Conn) {
 	// Build CER
+	// Passing nil as the last argument to NewRequest will use the default Parser
+	// and load dict.Default. To load other dictionaries you can use your own parser. e.g.
+	// parser, _ := dict.NewParser()
+	// parser.Load(bytes.NewReader(dict.DefaultXML))
+	// parser.Load(bytes.NewReader(dict.CreditControlXML))
+	// m := diam.NewRequest(257, 0, parser)
 	m := diam.NewRequest(257, 0, nil)
 	// Add AVPs
 	m.NewAVP("Origin-Host", 0x40, 0x00, Identity)

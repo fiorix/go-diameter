a987df1cd854032a12bf835628de167a99090260
Alexandre Fiori <fiorix@gmail.com>
2015-02-11 14:58:29 -0500

    Add doc to satisfy golint

--- diam/avp/doc.go
+++ diam/avp/doc.go
@@ -1,6 +1,6 @@
-// Copyright 2013-2014 go-diameter authors.  All rights reserved.
+// Copyright 2013-2015 go-diameter authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Diameter AVP codes and flags.
+// Package avp provides Diameter AVP constants and flags.
 package avp

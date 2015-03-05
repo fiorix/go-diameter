d7e7c36cfeef73bc92ecc9885f20e5011d5150b6
Alexandre Fiori <fiorix@gmail.com>
2015-03-05 03:03:54 -0500

    Fix docs

--- diam/sm/command/errors.go
+++ diam/sm/command/errors.go
@@ -10,29 +10,31 @@ import (
 )
 
 var (
-	// ErrMissingOriginHost is returned by CER.OK when the CER
-	// does not contain an Origin-Host AVP.
+	// ErrMissingOriginHost is returned by Parse when
+	// the message does not contain an Origin-Host AVP.
 	ErrMissingOriginHost = errors.New("missing Origin-Host")
 
-	// ErrMissingOriginRealm is returned by CER.OK when the CER
-	// does not contain an Origin-Realm AVP.
+	// ErrMissingOriginRealm is returned by Parse when
+	// the message does not contain an Origin-Realm AVP.
 	ErrMissingOriginRealm = errors.New("missing Origin-Realm")
 
-	// ErrMissingOriginStateID is returned by CER.OK when the CER
-	// does not contain an Origin-State-Id AVP.
+	// ErrMissingOriginStateID is returned by Parse when
+	// the message does not contain an Origin-State-Id AVP.
 	ErrMissingOriginStateID = errors.New("missing Origin-State-Id")
 
-	// ErrMissingApplication is returned by CER.OK when the CER
-	// does not contain any Acct-Application-Id or Auth-Application-Id,
-	// or their embedded versions in Vendor-Specific-Application-Id.
+	// ErrMissingApplication is returned by Parse when
+	// the CER does not contain any Acct-Application-Id or
+	// Auth-Application-Id, or their embedded versions in
+	// the Vendor-Specific-Application-Id AVP.
 	ErrMissingApplication = errors.New("missing application")
 
-	// ErrNoCommonSecurity is returned by CER.OK when the CER
-	// contains the Inband-Security-Id. We don't support that.
+	// ErrNoCommonSecurity is returned by Parse when
+	// the CER contains the Inband-Security-Id.
+	// We currently don't support that.
 	ErrNoCommonSecurity = errors.New("no common security")
 )
 
-// ErrNoCommonApplication is returned by CER.OK when the
+// ErrNoCommonApplication is returned by Parse when the
 // application IDs in the CER don't match the applications
 // defined in our dictionary.
 type ErrNoCommonApplication struct {

cad32e4943dde96d1d4a0e236b7a0b9d265f672c
Alexandre Fiori <fiorix@gmail.com>
2015-04-12 03:49:55 -0400

    Make CEA parser check acct/auth/vs applications
    This is required to make the peer package to create Context/metadata
    from the CEA, like it does from CER.

--- diam/sm/parser/cea.go
+++ diam/sm/parser/cea.go
@@ -12,10 +12,14 @@ import (
 // CEA is a Capabilities-Exchange-Answer message.
 // See RFC 6733 section 5.3.2 for details.
 type CEA struct {
-	ResultCode    uint32                    `avp:"Result-Code"`
-	OriginHost    datatype.DiameterIdentity `avp:"Origin-Host"`
-	OriginRealm   datatype.DiameterIdentity `avp:"Origin-Realm"`
-	OriginStateID uint32                    `avp:"Origin-State-Id"`
+	ResultCode                  uint32                    `avp:"Result-Code"`
+	OriginHost                  datatype.DiameterIdentity `avp:"Origin-Host"`
+	OriginRealm                 datatype.DiameterIdentity `avp:"Origin-Realm"`
+	OriginStateID               uint32                    `avp:"Origin-State-Id"`
+	AcctApplicationID           []*diam.AVP               `avp:"Acct-Application-Id"`
+	AuthApplicationID           []*diam.AVP               `avp:"Auth-Application-Id"`
+	VendorSpecificApplicationID []*diam.AVP               `avp:"Vendor-Specific-Application-Id"`
+	appID                       []uint32                  // List of supported application IDs.
 }
 
 // Parse parses and validates the given message.
@@ -26,6 +30,15 @@ func (cea *CEA) Parse(m *diam.Message) (err error) {
 	if err = cea.sanityCheck(); err != nil {
 		return err
 	}
+	app := &Application{
+		AcctApplicationID:           cea.AcctApplicationID,
+		AuthApplicationID:           cea.AuthApplicationID,
+		VendorSpecificApplicationID: cea.VendorSpecificApplicationID,
+	}
+	if _, err := app.Parse(m.Dictionary()); err != nil {
+		return err
+	}
+	cea.appID = app.ID()
 	return nil
 }
 
@@ -44,4 +57,9 @@ func (cea *CEA) sanityCheck() error {
 		return ErrMissingOriginStateID
 	}
 	return nil
+}
+
+// Applications return a list of supported Application IDs.
+func (cea *CEA) Applications() []uint32 {
+	return cea.appID
 }
--- diam/sm/parser/cea_test.go
+++ diam/sm/parser/cea_test.go
@@ -5,11 +5,13 @@
 package parser
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/fiorix/go-diameter/diam"
 	"github.com/fiorix/go-diameter/diam/avp"
 	"github.com/fiorix/go-diameter/diam/datatype"
+	"github.com/fiorix/go-diameter/diam/dict"
 )
 
 func TestCEA_MissingResultCode(t *testing.T) {
@@ -66,12 +68,72 @@ func TestCEA_MissingOriginStateID(t *testing.T) {
 	}
 }
 
+func TestCEA_MissingApplication(t *testing.T) {
+	m := diam.NewMessage(diam.CapabilitiesExchange, 0, 0, 0, 0, dict.Default)
+	m.NewAVP(avp.OriginHost, avp.Mbit, 0, datatype.DiameterIdentity("foobar"))
+	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, datatype.DiameterIdentity("test"))
+	m.NewAVP(avp.OriginStateID, avp.Mbit, 0, datatype.Unsigned32(1))
+	cer := new(CER)
+	_, err := cer.Parse(m)
+	if err == nil {
+		t.Fatal("Broken CER was parsed with no errors")
+	}
+	if err != ErrMissingApplication {
+		t.Fatal("Unexpected error:", err)
+	}
+}
+
+func TestCEA_NoCommonApplication(t *testing.T) {
+	m := diam.NewMessage(diam.CapabilitiesExchange, 0, 0, 0, 0, dict.Default)
+	m.NewAVP(avp.OriginHost, avp.Mbit, 0, datatype.DiameterIdentity("foobar"))
+	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, datatype.DiameterIdentity("test"))
+	m.NewAVP(avp.OriginStateID, avp.Mbit, 0, datatype.Unsigned32(1))
+	m.NewAVP(avp.AcctApplicationID, avp.Mbit, 0, datatype.Unsigned32(2))
+	cer := new(CER)
+	_, err := cer.Parse(m)
+	if err == nil {
+		t.Fatal("Broken CER was parsed with no errors")
+	}
+	appErr, ok := err.(*ErrNoCommonApplication)
+	if !ok {
+		t.Fatal("Unexpected error:", err.Error())
+	}
+	if appErr.ID != 2 {
+		t.Fatalf("Unexpected app ID. Want 2, have %d", appErr.ID)
+	}
+	if !strings.Contains(appErr.Error(), "acct application 2") {
+		t.Fatalf("Unexpected error message: %s", appErr)
+	}
+}
+
+func TestCEA_FailedAcctAppID(t *testing.T) {
+	m := diam.NewMessage(diam.CapabilitiesExchange, 0, 0, 0, 0, nil)
+	m.NewAVP(avp.ResultCode, avp.Mbit, 0, datatype.Unsigned32(diam.Success))
+	m.NewAVP(avp.OriginHost, avp.Mbit, 0, datatype.DiameterIdentity("foobar"))
+	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, datatype.DiameterIdentity("test"))
+	m.NewAVP(avp.OriginStateID, avp.Mbit, 0, datatype.Unsigned32(1))
+	m.NewAVP(avp.AcctApplicationID, avp.Mbit, 0, datatype.Unsigned32(1000))
+	cea := new(CEA)
+	err := cea.Parse(m)
+	if err == nil {
+		t.Fatal("Broken CER was parsed with no errors")
+	}
+	appErr, ok := err.(*ErrNoCommonApplication)
+	if !ok {
+		t.Fatal(err)
+	}
+	if appErr.ID != 1000 {
+		t.Fatalf("Unexpected app ID. Want 1000, have %d", appErr.ID)
+	}
+}
+
 func TestCEA(t *testing.T) {
 	m := diam.NewMessage(diam.CapabilitiesExchange, 0, 0, 0, 0, nil)
 	m.NewAVP(avp.ResultCode, avp.Mbit, 0, datatype.Unsigned32(diam.Success))
 	m.NewAVP(avp.OriginHost, avp.Mbit, 0, datatype.DiameterIdentity("foobar"))
 	m.NewAVP(avp.OriginRealm, avp.Mbit, 0, datatype.DiameterIdentity("test"))
 	m.NewAVP(avp.OriginStateID, avp.Mbit, 0, datatype.Unsigned32(1))
+	m.NewAVP(avp.AcctApplicationID, avp.Mbit, 0, datatype.Unsigned32(4))
 	cea := new(CEA)
 	if err := cea.Parse(m); err != nil {
 		t.Fatal(err)
@@ -82,5 +144,10 @@ func TestCEA(t *testing.T) {
 	}
 	if cea.OriginStateID != 1 {
 		t.Fatalf("Unexpected Result-Code. Want 1, have %d", cea.OriginStateID)
+	}
+	if app := cea.Applications(); len(app) != 1 {
+		if app[0] != 4 {
+			t.Fatalf("Unexpected app ID. Want 4, have %d", app[0])
+		}
 	}
 }

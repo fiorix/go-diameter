fiorix/go-diameter

commit 7b7598a955242b0551bb50b94dbc1bcb755509b8

Handle complex hosts and skip loopback IP (#145)
* Added support for complex local address formats

* Removed loopback IP and added tests for client.getLocalAddress()

* Return loopback interface IP from getLocalAddresses() if this is the only available interface

* Update client_test.go

Co-authored-by: emakeev <emakeev@gmail.com>

diam/sm/client.go
@@ -320,12 +320,25 @@ func (cli *Client) makeDWR(osid uint32) *diam.Message {
 	return m
 }
 
+func getHostsWithoutPort(hosts string) (string, error) {
+	i := len(hosts) - 1
+	for ; i >= 0 && hosts[i] != ':'; i-- {
+		if hosts[i] < '0' || hosts[i] > '9' {
+			return "", fmt.Errorf("found non numerical character in port at position %d", i+1)
+		}
+	}
+	return hosts[:i], nil
+}
+
 func getLocalAddresses(c diam.Conn) ([]datatype.Address, error) {
-	var addrStr string
+	var (
+		addrStr  string
+		loopback IP
+	)
 	if c.LocalAddr() != nil {
 		addrStr = c.LocalAddr().String()
 	}
-	addr, _, err := net.SplitHostPort(addrStr)
+	addr, err := getHostsWithoutPort(addrStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse local ip %s [%q]: %s", addrStr, c.LocalAddr(), err)
 	}
@@ -334,8 +347,15 @@ func getLocalAddresses(c diam.Conn) ([]datatype.Address, error) {
 	for _, ipStr := range hostIPs {
 		ip := net.ParseIP(ipStr)
 		if ip != nil {
-			addresses = append(addresses, datatype.Address(ip))
+			if ip.IsLoopback() {
+				loopback = ip
+			} else {
+				addresses = append(addresses, datatype.Address(ip))
+			}
 		}
+	}
+	if len(addresses) == 0 && loopback != nil {
+		addresses = append(addresses, datatype.Address(loopback))
 	}
 	return addresses, nil
 }

diam/datatype/time.go
@@ -19,17 +19,16 @@ const rfc2030offset = 2085978496 // 2085978496 comes from FFFFFFFF – 220898880
 
 // DecodeTime decodes a Time data type from byte array.
 func DecodeTime(b []byte) (Type, error) {
-        if len(b) != 4 {
-                return &Time{}, nil
-        }
-        if (b[0] >> 7) == 0 {
-                return Time(time.Unix(int64(binary.BigEndian.Uint32(b))+rfc2030offset, 0)), nil
-        } else {
-                return Time(time.Unix(int64(binary.BigEndian.Uint32(b))-rfc868offset, 0)), nil
-        }
+	if len(b) != 4 {
+		return &Time{}, nil
+	}
+	if (b[0] >> 7) == 0 {
+		return Time(time.Unix(int64(binary.BigEndian.Uint32(b))+rfc2030offset, 0)), nil
+	} else {
+		return Time(time.Unix(int64(binary.BigEndian.Uint32(b))-rfc868offset, 0)), nil
+	}
 
 }
-
 
 // Serialize implements the Type interface.
 func (t Time) Serialize() []byte {

diam/reflect_test.go
@@ -334,7 +334,7 @@ func TestEmbeddedStruct(t *testing.T) {
 	}
 	type CER struct {
 		Common
-		HostIP      net.IP `avp:"Host-IP-Address"`
+		HostIP net.IP `avp:"Host-IP-Address"`
 	}
 	var d CER
 	if err := msg.Unmarshal(&d); err != nil {
@@ -355,7 +355,6 @@ func TestEmbeddedStruct(t *testing.T) {
 		t.Fatal("Failed to marshal struct")
 	}
 
-
 	var readBack CER
 	if err = newMsg.Unmarshal(&readBack); err != nil {
 		t.Fatal(err)
@@ -363,7 +362,6 @@ func TestEmbeddedStruct(t *testing.T) {
 	if readBack.OriginHost != d.OriginHost {
 		t.Fatal("name does not match when mashal/unmarshalling")
 	}
-
 
 	// Test embedded non struct values
 	type CEREmb struct {
@@ -401,8 +399,6 @@ func TestEmbeddedStruct(t *testing.T) {
 	if readBackEmb.IP.String() != expectedIp.String() {
 		t.Fatalf("Host IP does not match when mashal/unmarshalling: %v != %v", readBackEmb.IP, expectedIp)
 	}
-
-
 
 }
 

diam/sm/client_test.go
@@ -5,6 +5,9 @@
 package sm
 
 import (
+	"context"
+	"crypto/tls"
+	"net"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -300,5 +303,67 @@ func TestClient_Watchdog_Timeout(t *testing.T) {
 	case <-c.(diam.CloseNotifier).CloseNotify():
 	case <-time.After(500 * time.Millisecond):
 		t.Fatal("Timeout waiting for watchdog to disconnect client")
+	}
+}
+
+// Type matching interface: net.Addr
+type testLocalAddr struct {
+	value string
+}
+
+func (a testLocalAddr) Network() string { return "tcp" }
+func (a testLocalAddr) String() string  { return a.value }
+
+// Type matching interface: diam.Conn
+type testLocalAddrDiamConn struct {
+	localAddr *testLocalAddr
+}
+
+func (d testLocalAddrDiamConn) Write(b []byte) (int, error)                    { return 0, nil }
+func (d testLocalAddrDiamConn) WriteStream(b []byte, stream uint) (int, error) { return 0, nil }
+func (d testLocalAddrDiamConn) Close()                                         {}
+func (d testLocalAddrDiamConn) LocalAddr() net.Addr                            { return d.localAddr }
+func (d testLocalAddrDiamConn) RemoteAddr() net.Addr                           { return nil }
+func (d testLocalAddrDiamConn) TLS() *tls.ConnectionState                      { return nil }
+func (d testLocalAddrDiamConn) Dictionary() *dict.Parser                       { return nil }
+func (d testLocalAddrDiamConn) Context() context.Context                       { return context.Background() }
+func (d testLocalAddrDiamConn) SetContext(c context.Context)                   {}
+func (d testLocalAddrDiamConn) Connection() net.Conn                           { return nil }
+
+func newTestLocalAddrDiamConn(localAddrValue string) diam.Conn {
+	return testLocalAddrDiamConn{
+		localAddr: &testLocalAddr{
+			value: localAddrValue,
+		},
+	}
+}
+
+func TestClient_Conn_LocalAddresses_Loopback(t *testing.T) {
+	c := newTestLocalAddrDiamConn("127.0.0.1:3868")
+
+	addrList, err := getLocalAddresses(c)
+	if err != nil {
+		t.Fatalf("Failed to parse local addresses: %v", err)
+	}
+	if len(addrList) != 1 {
+		t.Fatal("The only available loopback address was skipped")
+	}
+}
+
+func TestClient_Conn_LocalAddresses_Complex(t *testing.T) {
+	c := newTestLocalAddrDiamConn("127.0.0.1/[::1%lo]/10.0.0.3/[fe80::78ef:0efb:a57b:15b9%eth0]:3868")
+
+	addrList, err := getLocalAddresses(c)
+	if err != nil {
+		t.Fatalf("Failed to parse local addresses: %v", err)
+	}
+	if len(addrList) != 1 {
+		t.Fatal("Failed to parse valid IP address or failed to skip loopback")
+	}
+
+	actual := net.IP(addrList[0]).String()
+	expected := "10.0.0.3"
+	if actual != expected {
+		t.Fatalf("Wrong IP address found in list of local addresses, expected: %s, actual: %s", expected, actual)
 	}
 }

commit 002a72d447a2eddcc19887b3d2cc649063011568

diam/sm: fix "undefined: IP" error
Fixes: 7b7598a95524 ("Handle complex hosts and skip loopback IP (#145)")

diam/sm/client.go
@@ -333,7 +333,7 @@ func getHostsWithoutPort(hosts string) (string, error) {
 func getLocalAddresses(c diam.Conn) ([]datatype.Address, error) {
 	var (
 		addrStr  string
-		loopback IP
+		loopback net.IP
 	)
 	if c.LocalAddr() != nil {
 		addrStr = c.LocalAddr().String()
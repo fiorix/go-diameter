2c5bcffc56e3462dbccdabb45596d21d83d4205b
Nishant <nishant.d.sharma@capgemini.com>
2025-03-27 08:16:48 +0000

    imports changed as per dir name

--- diam/pretty_dump.go
+++ diam/pretty_dump.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/fiorix/go-diameter/v4/diam/avp"
-	"github.com/fiorix/go-diameter/v4/diam/datatype"
-	"github.com/fiorix/go-diameter/v4/diam/dict"
+	"github.com/deb-2025/go-diameter/diam/avp"
+	"github.com/deb-2025/go-diameter/diam/datatype"
+	"github.com/deb-2025/go-diameter/diam/dict"
 )
 
 func (m *Message) PrettyDump() string {
